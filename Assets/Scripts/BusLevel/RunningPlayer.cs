3e3ce09b33b0d134570452071cb10a6aaf5c3f87
Emanuele De Donatis <emanuelededonatis@Emanueles-MacBook-Air.local>
2014-11-28 17:11:27 +0100

    [BusLevel] TapGesture

---
+++ Assets/Scripts/BusLevel/RunningPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using TouchScript.Gestures;
+
+public class RunningPlayer : MonoBehaviour {
+
+	public GameObject left_btn;
+	public GameObject right_btn;
+
+	public float deltaX = 1;
+
+	bool left_pressed = true;
+	bool right_pressed = false;
+
+	private void OnEnable()
+	{
+		// subscribe to gesture's Tapped event
+		left_btn.GetComponent<TapGesture>().Tapped += tapLeft;
+		right_btn.GetComponent<TapGesture>().Tapped += tapRight;
+		
+	}
+	
+	private void OnDisable()
+	{
+		// don't forget to unsubscribe
+		left_btn.GetComponent<TapGesture>().Tapped -= tapLeft;
+		right_btn.GetComponent<TapGesture>().Tapped -= tapRight;
+	}
+
+	private void tapLeft(object sender, EventArgs e)
+	{
+		if(right_pressed){
+			move();
+			right_pressed = false;
+			left_pressed = true;
+		}
+	}
+
+	private void tapRight(object sender, EventArgs e)
+	{
+		if(left_pressed){
+			move();
+			right_pressed = true;
+			left_pressed = false;
+		}
+	}
+
+	private void move() {
+		Vector2 pos = transform.position;
+		pos.x += deltaX;
+		transform.position = pos;
+	}
+}
