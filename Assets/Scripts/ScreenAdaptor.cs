1752932e32b653dda0dde5d8db4f7f802c1da4b9
Emanuele De Donatis <emanuelededonatis@Emanueles-MacBook-Air.local>
2014-12-02 13:16:04 +0100

    Screen Adaptor

---
+++ Assets/Scripts/ScreenAdaptor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenAdaptor : MonoBehaviour {
+
+	public GameObject[] p1;
+	public GameObject[] p2;
+	public GameObject[] p3;
+	public GameObject[] p4;
+	
+	void Start () {
+		int num_players = GameManager.Instance.getNumPlayer();
+		switch(num_players) {
+			case 1: foreach(GameObject obj in p2)
+						obj.SetActive(false);
+					goto case 2;
+			case 2: foreach(GameObject obj in p3)
+						obj.SetActive(false);
+					goto case 3;
+			case 3: foreach(GameObject obj in p4)
+						obj.SetActive(false);
+					break;
+		}
+		
+	}
+}
