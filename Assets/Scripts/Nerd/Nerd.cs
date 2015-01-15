a547d979de24db139d115ae69454b5c0f3c34f48
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2015-01-15 19:22:29 +0100

    max value nerd

---
+++ Assets/Scripts/Nerd/Nerd.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nerd : MonoBehaviour {
+	
+	
+	private GameManager.ePlayers player;
+	private LevelManager lvm;
+	private NerdLevelManager alvm;
+	private bool shooted = false;
+
+	float dir;
+	
+	void Start () {
+		lvm = GameObject.Find("LevelManager").GetComponent<LevelManager>() as LevelManager;
+		alvm = GameObject.Find("NerdLevelManager").GetComponent<NerdLevelManager>() as NerdLevelManager;
+		rigidbody2D.gravityScale = 0;
+		//transform.Rotate (Vector3.forward, Time.deltaTime * 180, Space.Self);
+	}
+
+	void Update() {
+		if (shooted) {
+			rigidbody2D.gravityScale = 100;
+			rigidbody2D.AddForce(transform.up * 900, ForceMode2D.Impulse);
+			shooted = false;
+		}
+	}
+
+	public void setPlayer(GameManager.ePlayers player) {
+		this.player = player;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.name == "floor"+(player.GetHashCode()+1)) {
+			rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+
+
+	public bool Shooted {
+		get {
+			return shooted;
+		}
+		set {
+			shooted = value;
+		}
+	}
+}
---
+++ Assets/Scripts/Nerd/NerdPlayer.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NerdPlayer : MonoBehaviour {
+	
+	public GameManager.ePlayers player;
+	private GameManager.eColors color;
+
+	private GameObject button;
+	private GameObject nerdPrefab;
+	
+	private float force;
+	private Vector3 direction;
+	private GameObject nerdInstance;
+
+	private NerdLevelManager sceneMgr;
+	private LevelManager lvm;
+	
+	private Animator animator;
+	private RuntimeAnimatorController animCtrl;
+	
+	void Awake() {
+		sceneMgr = GameObject.Find("NerdLevelManager").GetComponent<NerdLevelManager>() as NerdLevelManager;
+		lvm = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+	}
+	
+	void Start () {
+		button = GameObject.Find ("UIManager").GetComponent<UIManager> ().getButton (player);
+		try {
+			color = GameManager.Instance.getColor(player);
+			animCtrl = Resources.Load <RuntimeAnimatorController> ("Sprites/Characters/" + color.ToString() + "/animation/" + color.ToString() + "_bucket");
+			animator = GetComponent<Animator>();			
+			animator.runtimeAnimatorController = animCtrl;
+			nerdPrefab = Resources.Load <GameObject> ("Prefabs/Nerd");
+
+		}catch{
+			gameObject.SetActive(false);
+		}
+		button.GetComponent<BtnHandler>().OnPressed += shoot;
+		lvm.OnFinish += endPlayer;
+		lvm.OnStart += StartPlayer;
+	}
+
+	void StartPlayer() {
+		nerdInstance = Instantiate(nerdPrefab, transform.position - new Vector3(20f, 20f, 0), Quaternion.Euler(new Vector3(0, 0, -45f))) as GameObject;
+		animator.SetBool("isLoading", true);
+
+		}
+
+	//pencilInstance.transform.Rotate (Vector3.forward, Time.deltaTime * 180, Space.Self);
+
+	
+	private void shoot() {
+		if(lvm.getState() == LevelManager.eState.Run) {
+			animator.SetBool("isLoading", false);
+			animator.SetBool("isShooting", true);
+			StartCoroutine(waitAnimation());
+		}
+	}
+
+	private IEnumerator waitAnimation() {
+		yield return new WaitForSeconds(0.1f);
+		try{
+			nerdInstance.GetComponent<Nerd>().Shooted = true;
+		}catch{
+		}
+		yield return new WaitForSeconds(1f);
+	}
+	
+	public void endShooting() {
+		animator.SetBool("isShooting",false);
+	}
+	
+	public void endPlayer() {
+		//IF NOT LAST PLAYER
+		int num_players = GameManager.Instance.getNumPlayer ();
+		if (num_players == 1 || lvm.getPodium (num_players - 1) != this.player) {
+			//IF SINGLE PLAYER OR NOT LAST PLAYER
+			animCtrl = Resources.Load <RuntimeAnimatorController> ("Sprites/Podium/" + color.ToString () + "_podium_winner");
+		} else {
+			animCtrl = Resources.Load <RuntimeAnimatorController> ("Sprites/Podium/" + color.ToString () + "_podium_loser");
+		}
+		animator = GetComponent<Animator>();			
+		animator.runtimeAnimatorController = animCtrl;
+	}
+}
