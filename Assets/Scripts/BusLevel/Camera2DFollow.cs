ee4b8158bdded15abd2f3f05e4b3cf7eaccee3fb
Emanuele De Donatis <emanuele.dedonatis@gmail.com>
2015-01-15 02:32:13 +0100

    minor fix

--- Assets/Scripts/BusLevel/Camera2DFollow.cs
+++ Assets/Scripts/BusLevel/Camera2DFollow.cs
@@ -7,17 +7,26 @@ public class Camera2DFollow : MonoBehaviour {
 
 	void Start() {
 		player = new GameObject[4];
-		for (int i=0; i<4; i++)
-			player[i] = GameObject.Find ("Player" + (i+1));
+		for (int i=0; i<4; i++) {
+			try{
+				player [i] = GameObject.Find ("Player" + (i + 1));
+			}catch(System.NullReferenceException e){
+				//player disabled
+			}
+				}
 	}
 	
 	void Update () {
 		float max_x = 0;
 		foreach (GameObject p in player) {
-						if (p.transform.position.x > max_x) {
-								max_x = p.transform.position.x;
-						}
-				}
+			try{
+						if (p.transform.position.x > max_x) 
+				max_x = p.transform.position.x;
+			}catch(System.NullReferenceException e){
+				//player disabled
+			}
+				
+		}
 
 		Vector3 newPos = new Vector3 (max_x, 0, -10f);
 		transform.position = Vector3.Lerp (transform.position, newPos, 10 * Time.deltaTime);
